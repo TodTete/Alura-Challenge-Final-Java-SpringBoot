c7747037575205da81a6ab889ca7bb5775473df2
TodTete <vallejoricardo3@gmail.com>
2026-01-21 18:17:41 -0600

    New File #1

---
+++ src/main/java/com/challenge/alura/ChallengeAluraFinal/ChallengeAluraFinalApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.alura.ChallengeAluraFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAluraFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAluraFinalApplication.class, args);
+	}
+
+}
